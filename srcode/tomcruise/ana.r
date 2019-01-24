f446eec1bebd9c8e72f3ba5b7e631ecb2cf81894
peter159 <destination1986@163.com>
2019-01-24 20:14:01 +0800

    upd

--- srcode/tomcruise/ana.r
+++ srcode/tomcruise/ana.r
@@ -20,30 +20,32 @@ Results <- sapply(Results,function(x) strsplit(x, ",")) %>%
 colnames(Results) <- c("word","frequency")
 Results
 
-ggplot2::ggplot(Results, aes(x=word, y=frequency, fill=word)) +
-  geom_bar(width = 0.75,
-           stat = "identity",
-           colour = "black",
-           size = 1) +
-  coord_polar(theta = "x") +
-  xlab("") + ylab("") +
-  ggtitle("Movie Theme Distribution") +
-  theme(legend.position = "none",
-        plot.title = element_text(hjust = 0.5,size = 10),
-        text = element_text(size = 20),
-        axis.text.y = element_text(size = 1)) +
-  labs(x = NULL, y = NULL)
+ggplot_wordcount_polar(Results,"word","frequency")
 
-plotly::ggplotly(ggplot2::ggplot(Results, aes(x=word, y=frequency, fill=word)) +
-                   geom_bar(width = 0.75, stat = "identity", colour = "black", size = 1) +
-                   xlab("") +
-                   ylab("") +
-                   ggtitle("Word Frequency") +
-                   theme(legend.position = "none") +
-                   dllabs(x = NULL, y = NULL) +
-                   theme(plot.subtitle = element_text(vjust = 1),
-                         plot.caption = element_text(vjust = 1),
-                         axis.text.x = element_text(angle = 90)) +
-                   theme(panel.background = element_rect(fill = "honeydew1"),
-                         plot.backgrond = element_rect(fill = "antiquewhite"))) %>%
-  config(displaylogo = F) %>% config(showLink = F)
+## ggplot2::ggplot(Results, aes(x=word, y=frequency, fill=word)) +
+##   geom_bar(width = 0.75,
+##            stat = "identity",
+##            colour = "black",
+##            size = 1) +
+##   coord_polar(theta = "x") +
+##   xlab("") + ylab("") +
+##   ggtitle("Movie Theme Distribution") +
+##   theme(legend.position = "none",
+##         plot.title = element_text(hjust = 0.5,size = 10),
+##         text = element_text(size = 20),
+##         axis.text.y = element_text(size = 1)) +
+##   labs(x = NULL, y = NULL)
+
+## plotly::ggplotly(ggplot2::ggplot(Results, aes(x=word, y=frequency, fill=word)) +
+##                    geom_bar(width = 0.75, stat = "identity", colour = "black", size = 1) +
+##                    xlab("") +
+##                    ylab("") +
+##                    ggtitle("Word Frequency") +
+##                    theme(legend.position = "none") +
+##                    labs(x = NULL, y = NULL) +
+##                    theme(plot.subtitle = element_text(vjust = 1),
+##                          plot.caption = element_text(vjust = 1),
+##                          axis.text.x = element_text(angle = 90)) +
+##                    theme(panel.background = element_rect(fill = "honeydew1"),
+##                          plot.background = element_rect(fill = "antiquewhite"))) %>%
+##   config(displaylogo = F) %>% config(showLink = F)
