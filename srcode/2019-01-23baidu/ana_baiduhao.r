1c743c5d514597c36f75d4f315cd814905a20990
peter159 <peter.linyi159@gmail.com>
2022-05-24 10:35:08 +0800

    refresh

--- srcode/2019-01-23baidu/ana_baiduhao.r
+++
@@ -1,8 +0,0 @@
-library(dapre)
-
-data <- fread("./keyword_ratio.csv")
-datainfo(data)
-data
-
-data <- data.table(ratio = rnorm(100,0.7,0.1) + runif(100, -0.1,0),tag = 1)
-ggplot_continuous_hist(data,"ratio")
